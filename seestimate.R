d73f2d272b6b028c20b24efc854efb5bfa5d7c66
wj107 <will.johnson@gmail.com>
2018-01-24 23:14:17 -0600

    check for arguments is set.  next step:  system.time and variable data

--- seestimate.R
+++ seestimate.R
@@ -16,34 +16,42 @@
 se.estimate<-function(
 	####number of sample groups to draw		
 		resamples=100,
-	#####size of sample groups
-		resample.size=500,
 	#####pct of data used to re-sample. will override resample.size if given 
 		resample.pct=NULL,		
-	###percentage of data to be used as 'train' data set -- default 70%
+	#####size of re-sample groups (valid only if resample.pct is not defined)
+		resample.size=500,
+	###percentage of re-sample data to be used as 'train' data set -- default 70%
 		train.pct=0.70,
-	###select method used to calculate standard error
-		method="both")
+	###select method(s) used to calculate standard error -- "glm", "nn", "both"
+		method="both",
+	###progress bar?  T or F
+		prog.bar=T)
 	{
-
-####possible methods; make sure 'method' arg is valid
-method.options<-c("glm","nn","both")
-##if(!(method %in% method.options)) {stop}
 
 ######define data set
 dat<-diamonds
-N<-nrow(diamonds)
-
-##########checking arguments else stop
-##if(!is.integer(resamples)) {stop}
-if(!is.null(resample.pct)) resample.size<-round(N*resample.pct)
-##if(!is.integer(resample.size)) {stop}
-##if(!(resample.size>=0 && resample.size<=N)) {stop}
-##if(!(train.pct>=0 && 1>=train.pct)) {stop}
-
-
+N<-nrow(dat)
 ######define response variable
 pred.var<-"price"
+
+#######check argument:  'resamples'
+if(!(resamples==round(resamples) && resamples>0)) stop("Argument `resamples' must be a positive integer")
+#######check argument:  'resample.pct'  does it exist?  is it valid?  then, override resample.size
+if(!is.null(resample.pct)) {
+	if(!(resample.pct>0 && resample.pct<=1)) stop("Argument `resample.pct' must be a value greater than zero and less than or equal to 1")
+	resample.size<-round(N*resample.pct)
+	}
+#######check argument:  'resample.size'
+if(!(resample.size==round(resample.size) && resample.size>0)) stop("Argument `resample.size' must be a positive integer")
+if(resample.size>N) stop("Argument `resample.size' exceeds rows in data set")
+#######check argument:  'train.pct'
+if(!(train.pct>0 && train.pct<1)) stop("Argument `train.pct' must be a value between zero and one, exclusive")
+#######possible methods; make sure 'method' arg is valid
+method.options<-c("glm","nn","both")
+if(!(method %in% method.options)) stop("Argument `method' must be one of `glm', `nn', or `both'")
+#######check argument: 'prog.bar'
+if(!is.logical(prog.bar)) stop("Argument `prog.bar' must be logical")
+
 
 ##########prep data
 	###get variable names
@@ -73,37 +81,37 @@ pred.var<-"price"
 ################################
 	######check if glm is tested
 	if(method=="glm" || method=="both"){
-
-	####initialize output variable
-	glm.se.estimate<-NULL
-	####initialize progress bar (!)	
-	##name="Re-sampling data K times to calculate standard error of GLM model"
-	pbar<-create_progress_bar('text')
-	pbar$init(resamples)
-
-	#######train/test/compute SE  "groups" number of times
-	for(i in 1:resamples){
-		###find index for re-sampling set
-		resample<-sample(N,resample.size)
-		###divide re-sample into test/train index
-		train.index<-sample(resample.size,round(train.pct*resample.size))
-		####define train/set from data
-		train<-dat[resample[train.index],]
-		test<-dat[resample[-train.index],]
+		####initialize output variable
+		glm.se.estimate<-NULL
+		#####if prog.bar=T, initialize progress bar
+		if(prog.bar){
+			print("Re-sampling data, creating and testing models, to calculate standard error for a GLM model")
+			progress.bar<-create_progress_bar('text')
+			progress.bar$init(resamples)
+			}
+		#######train/test/compute SE  "resamples" number of times
+		for(i in 1:resamples){
+			###find index for re-sampling set
+			resample.index<-sample(N,resample.size)
+			###divide re-sample into test/train index
+			train.index<-sample(resample.size,round(train.pct*resample.size))
+			####define train/set from data
+			train<-dat[resample.index[train.index],]
+			test<-dat[resample.index[-train.index],]
 		
-		####create glm!!
-		glm.model<-glm(model.formula,,train)
-		####predict from model:
-		pred.glm<-predict(glm.model,test)
-		####actual responses
-		resp.glm<-test[,resp.col]
-		####calculate standard error
-		glm.se.estimate[i]<-sum((pred.glm-resp.glm)^2)/nrow(test)
-		####step up
-		pbar$step()
+			####create glm!!
+			glm.model<-glm(model.formula,,train)
+			####predict from model:
+			pred.glm<-predict(glm.model,test)
+			####actual responses
+			resp.glm<-test[,resp.col]
+			####calculate standard error
+			glm.se.estimate[i]<-sum((pred.glm-resp.glm)^2)/nrow(test)
+			####if prog.bar=T, step up
+			if(prog.bar) progress.bar$step()
+			}
+		##end glm estimate
 		}
-	##end glm estimate
-	}
 
 
 ###############################
@@ -111,37 +119,38 @@ pred.var<-"price"
 ###############################
 	######check if nn is tested
 	if(method=="nn" || method=="both"){
-	####initialize output variable
-	nn.se.estimate<-NULL
-	####initialize progress bar (!)	
-	##name="Re-sampling data K times to calculate standard error of NN model"
-	pbar<-create_progress_bar('text')
-	pbar$init(resamples)
-
-	#######train/test/compute SE  "groups" number of times
-	for(i in 1:resamples){
-		###find index for re-sampling set
-		resample<-sample(N,resample.size)
-		###divide re-sample into test/train index
-		train.index<-sample(resample.size,round(train.pct*resample.size))
-		####define train/set from scaled data
-		train<-dat.scaled[resample[train.index],]
-		test<-dat.scaled[resample[-train.index],]
+		####initialize output variable
+		nn.se.estimate<-NULL
+		#####if prog.bar=T, initialize progress bar
+		if(prog.bar){
+			print("Re-sampling data, creating and testing models, to calculate standard error for a NN model")
+			progress.bar<-create_progress_bar('text')
+			progress.bar$init(resamples)
+			}
+		#######train/test/compute SE  "resamples" number of times
+		for(i in 1:resamples){
+			###find index for re-sampling set
+			resample.index<-sample(N,resample.size)
+			###divide re-sample into test/train index
+			train.index<-sample(resample.size,round(train.pct*resample.size))
+			####define train/set from scaled data
+			train<-dat.scaled[resample.index[train.index],]
+			test<-dat.scaled[resample.index[-train.index],]
 		
-		####create neuralnet!!
-		nn<-neuralnet(model.formula,train,hidden=c(10,10,10),linear.output=F)
-		####predict, and re-scale:
-		pred.nn<-compute(nn,test[,-resp.col])
-		pred.nn<-pred.nn$net.result*(resp.max-resp.min)+resp.min
-		####actual responses
-		resp.nn<-test[,resp.col]*(resp.max-resp.min)+resp.min
-		####calculate standard error
-		nn.se.estimate[i]<-sum((resp.nn-pred.nn)^2)/nrow(test)
-		####step up
-		pbar$step()
+			####create neuralnet!!
+			nn<-neuralnet(model.formula,train,hidden=c(10,10,10),linear.output=F)
+			####predict, and re-scale:
+			pred.nn<-compute(nn,test[,-resp.col])
+			pred.nn<-pred.nn$net.result*(resp.max-resp.min)+resp.min
+			####actual responses
+			resp.nn<-test[,resp.col]*(resp.max-resp.min)+resp.min
+			####calculate standard error
+			nn.se.estimate[i]<-sum((resp.nn-pred.nn)^2)/nrow(test)
+			####if prog.bar=T, step up
+			if(prog.bar) progress.bar$step()
+			}
+		###end nn estimates
 		}
-	###end nn estimates
-	}
 
 
 #####################
--- sestudy.R
+++ sestudy.R
@@ -9,10 +9,12 @@
 se.study.2d<-function(
 	####select re-sampling parameter to vary
 		param="resample.size",
+	####number of resamples in each step
+		resamples=25,
 	####select minimum value for parameter
-		param.min,
+		param.min=10,
 	####select maximum value for parameter
-		param.max,
+		param.max=100,
 	####select number of steps for parameter
 		param.step.number=10,
 	####select step value for parameter  ...will override param.step.number if given
@@ -25,13 +27,11 @@ se.study.2d<-function(
 ###possible methods:  ensure 'method' arg is valid
 method.options<-c("glm", "nn", "both")
 ##if(!(method %in% method.options)) {stop}
-if(!is.null(param.step.value)) param.step.number<-round((param.max-param.min)/param.step.value)
+###stop if param.step.value is > than param.max-param.min
+if(!is.null(param.step.value)) param.step.number<-round((param.max-param.min)/param.step.value)+1
 
 ####define the steps for parameter to study
 param.steps<-round(seq(param.min,param.max,,param.step.number))
-#####how big is each resample??
-resamples<-100
-
 
 #####initialize output variable
 resample.sizes<-rep(param.steps,each=resamples)
@@ -42,9 +42,9 @@ pbar<-create_progress_bar("text")
 pbar$init(param.step.number)
 
 #####loop through steps, call seestimate for each step
-for(i in param.step.number){
+for(i in 1:param.step.number){
 	param.value<-param.steps[i]
-	se.estimate(resamples,resample.size=param.value,train.pct=0.70,method=method)[[1]]->standard.error[[i]]
+	se.estimate(resamples,resample.size=param.value,train.pct=0.70,method=method,prog.bar=F)[[1]]->standard.error[[i]]
 	pbar$step()
 	}
 
